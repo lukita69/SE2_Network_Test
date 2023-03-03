5af686190f8ef4f3a49a76b4f491e7ace43c1962
Luka Kravos <lukakr@edu.aau.at>
2023-03-03 12:34:22 +0100

    implemented inner class to handle multiple Clients

--- app/src/main/java/com/example/networktest/Server.kt
+++ app/src/main/java/com/example/networktest/Server.kt
@@ -1,9 +1,12 @@
 package com.example.networktest
 
+import java.io.DataInputStream
+import java.io.DataOutputStream
 import java.net.ServerSocket
 import java.net.Socket
+import java.nio.ByteBuffer
 
-class Server(private val port: Int): Thread() {
+class Server(private val port: Int) : Thread() {
 
     override fun run() {
         super.run()
@@ -14,14 +17,39 @@ class Server(private val port: Int): Thread() {
                 val clientThread = ClientThread(clientSocket)
                 clientThread.start()
             }
-        }catch (e: Exception){
+        } catch (e: Exception) {
             e.printStackTrace()
         }
     }
 
-    inner class ClientThread(private val clientSocket: Socket):Thread(){
+    inner class ClientThread(private val clientSocket: Socket) : Thread() {
+        //val bufferSize: Int = 1024
+
         override fun run() {
             super.run()
+            try {
+                val inputStream: DataInputStream = DataInputStream(clientSocket.getInputStream())
+                val outputStream: DataOutputStream =
+                    DataOutputStream(clientSocket.getOutputStream())
+
+                //val receivedBytes:ByteArray = ByteArray(bufferSize)
+
+                val matrikelNummer: Long =
+                    inputStream.readLong() //ByteBuffer.wrap(receivedBytes).long
+                val ergebnis: Long = calcMatNr(matrikelNummer);
+
+                outputStream.writeLong(ergebnis)
+                outputStream.flush()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                clientSocket.close();
+            }
         }
+    }
+
+    private fun calcMatNr(matNr: Long): Long {
+
+        return matNr;
     }
 }
